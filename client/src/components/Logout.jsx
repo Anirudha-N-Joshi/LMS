284f4ad65aaea6c3be7b4166a0c36d836d04e2d7
AnanthRitti <rittiananth@gmail.com>
2024-04-28 10:50:39 +0530

    logout updated

--- client/src/components/Logout.jsx
+++ client/src/components/Logout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useCookies } from 'react-cookie';
 
 const Logout = () => {
-    const [cookies, setCookie, removeCookie] = useCookies(['authToken', 'userId', 'user_type']);
+    const [cookies, setCookie, removeCookie] = useCookies();
 
     function logout() {
   
